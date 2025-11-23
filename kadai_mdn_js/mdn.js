d1edcda876d7875080b756bf8507f2b0786ce6de
Hitomi <hitomi.muramatsu219@gmail.com>
2025-11-23 17:59:37 +0800

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,6 @@
+const today = new Date();
+const fullYear = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log(fullYear + "年" + month + "月" + day + "日"); 
